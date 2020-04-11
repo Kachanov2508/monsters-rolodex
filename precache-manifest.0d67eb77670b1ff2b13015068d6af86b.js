26935c99f61e59135604deaeeb3d8b81d66280db
Kachanov2508 <kachanov2508@yandex.ru>
2020-04-11 16:48:36 +0300

    Updates

---
+++ precache-manifest.0d67eb77670b1ff2b13015068d6af86b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dfa4197fd6e6cfecd028811aa0c3fa9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c3a06b57b9f70869020a",
+    "url": "/monsters-rolodex/static/css/main.eb28ce38.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3a06b57b9f70869020a",
+    "url": "/monsters-rolodex/static/js/main.3835d558.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
